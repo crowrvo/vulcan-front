24b81c27ee529b6b5664d12c1563b66f2ffeef70
devdouglassilva <devdouglassilva@gmail.com>
2024-03-04 17:59:28 +0000

    Added g-input

---
+++ components/global/g-input.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+    type: string
+}>(), { type: 'text' });
+
+const value = defineModel('value', {
+    type: String
+})
+const checked = defineModel('checked', {
+    type: Boolean
+})
+</script>
+<template>
+    <input v-if="type !== 'checkbox' && type !== 'radio'" :type="type" v-model="value"
+        class="max-lg:py-3 px-3 py-4 rounded-sd bg-neutral-g30 dark:bg-neutral-g500 caret-neutral-g900 dark:caret-neutral-g0 text-neutral-dark dark:text-white text-base lg:text-md" />
+    <input v-else v-model="checked" :type="type"
+        class="p-3 rounded-sd bg-neutral-g30 dark:bg-neutral-g500 accent-primary-600 dark:accent-primary-400 text-base lg:text-md" />
+</template>
