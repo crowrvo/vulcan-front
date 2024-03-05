8d8b3071124832596ef1d87d93b1ebe2e9efc316
devdouglassilva <devdouglassilva@gmail.com>
2024-03-05 10:07:06 +0000

    Change(color-mode):Appearance and icon provider Using <g-icon /> component instead of Icon

--- components/global/g-color-mode.vue
+++ components/global/g-color-mode.vue
@@ -6,12 +6,12 @@ const toggleColorMode = useToggle(colorMode)
 </script>
 <template>
     <div
-        class="bg-neutral-white-50 dark:bg-neutral-g900 inline-flex gap-3 px-3 py-2 text-secundary-950 dark:text-secundary-300 rounded-sd">
-        <Icon name="mdi:weather-sunny" class="text-current size-6" />
+        class="bg-secundary-50 dark:bg-neutral-g900 inline-flex items-center gap-3 px-3 py-2 text-secundary-950 dark:text-secundary-50 rounded-sd">
+        <g-icon icon="mdi:weather-sunny" class="text-current text-sm lg:text-lg" />
         <button tabindex="0" type="button" @click="toggleColorMode()"
-            class="relative !bg-secundary-200 dark:bg-secundary-950 w-14 rounded-full focus:outline-2 !p-[2px]">
-            <div class="transition duration-300 ease-in bg-secundary-600 size-6 rounded-full" :class="[colorMode ? 'translate-x-[calc(100%_+_4px)]': '' ]"></div>
+            class="!bg-secundary-200 dark:bg-secundary-950 w-14 rounded-full focus:outline-2 !p-[2px]">
+            <div class="transition duration-300 ease-in bg-secundary-600 size-6 rounded-full" :class="[colorMode ? 'translate-x-7': '' ]"></div>
         </button>
-        <Icon name="mdi:weather-night" class="text-current size-6" />
+        <g-icon icon="mdi:weather-night" class="text-current text-sm lg:text-lg" />
     </div>
 </template>
