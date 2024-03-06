3182f7a361f92eb778b2530cf162268f6eb8eebd
devdouglassilva <devdouglassilva@gmail.com>
2024-03-06 20:24:53 +0000

    Update(input field): Update Input field in Field Update the border radius of the input field using important modifier

--- components/global/g-input.vue
+++ components/global/g-input.vue
@@ -12,7 +12,7 @@ const checked = defineModel('checked', {
 </script>
 <template>
     <input v-if="type !== 'checkbox' && type !== 'radio'" :type="type" v-model="value"
-        class="max-lg:py-3 px-3 py-4 rounded-sd bg-neutral-g30 dark:bg-neutral-g500 caret-neutral-g900 dark:caret-neutral-g0 text-neutral-dark dark:text-white text-base lg:text-md" />
+        class="max-lg:py-3 px-3 py-4 rounded-sd bg-neutral-g30 dark:bg-neutral-g500 caret-neutral-g900 dark:caret-neutral-g0 text-neutral-dark dark:text-white text-base lg:text-md focus:outline focus:outline-secundary-400" />
     <input v-else v-model="checked" :type="type"
-        class="p-3 rounded-sd bg-neutral-g30 dark:bg-neutral-g500 accent-primary-600 dark:accent-primary-400 text-base lg:text-md" />
+        class="p-3 rounded-sd bg-neutral-g30 dark:bg-neutral-g500 accent-primary-600 dark:accent-primary-400 text-base lg:text-md focus:outline focus:outline-secundary-400" />
 </template>
