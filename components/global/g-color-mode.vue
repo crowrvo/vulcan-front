c31999a5155342289299206fbeb4ab4dde745a0e
devdouglassilva <devdouglassilva@gmail.com>
2024-03-07 17:55:49 +0000

    Update(toggle color mode): Add emiter show-menu Add emiter for show the menu of the user

--- components/global/g-color-mode.vue
+++ components/global/g-color-mode.vue
@@ -8,9 +8,9 @@ const toggleColorMode = useToggle(colorMode)
     <div
         class="bg-secundary-50 dark:bg-neutral-g900 inline-flex items-center gap-3 px-3 py-2 text-secundary-950 dark:text-secundary-50 rounded-sd">
         <g-icon icon="mdi:weather-sunny" class="text-current text-sm lg:text-lg" />
-        <button tabindex="0" type="button" @click="toggleColorMode()"
-            class="!bg-secundary-200 dark:bg-secundary-950 w-14 rounded-full focus:outline-2 !p-[2px]">
-            <div class="transition duration-300 ease-in bg-secundary-600 size-6 rounded-full" :class="[colorMode ? 'translate-x-7': '' ]"></div>
+        <button tabindex="0" type="button" @click="()=>{toggleColorMode(); $emit('show-menu', true)}" @focus="$emit('show-menu', true)"
+            class="!bg-secundary-200 dark:!bg-neutral-g700 w-14 rounded-full focus:outline-2 focus:outline-secundary-500 dark:focus:outline-secundary-900 focus:border-0 !p-[2px]">
+            <div @focus="$emit('show-menu', true)" class="transition duration-300 ease-in bg-secundary-600 size-6 rounded-full" :class="[colorMode ? 'translate-x-7': '' ]"></div>
         </button>
         <g-icon icon="mdi:weather-night" class="text-current text-sm lg:text-lg" />
     </div>
