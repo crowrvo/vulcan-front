f1b146badfb437d4b717f8d995ac55a3b5086078
devdouglassilva <devdouglassilva@gmail.com>
2024-03-11 13:27:49 +0000

    Change(props): Add props Add props labelBellow and inputName; Add props name to g-input

--- components/global/g-input.vue
+++ components/global/g-input.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 withDefaults(defineProps<{
-    type: string
+    type: string,
+    name?: string;
 }>(), { type: 'text' });
 
 const value = defineModel('value', {
@@ -11,8 +12,8 @@ const checked = defineModel('checked', {
 })
 </script>
 <template>
-    <input v-if="type !== 'checkbox' && type !== 'radio'" :type="type" v-model="value"
-        class="max-lg:py-3 px-3 py-4 rounded-sd bg-neutral-g30 dark:bg-neutral-g500 caret-neutral-g900 dark:caret-neutral-g0 text-neutral-dark dark:text-white text-base lg:text-md focus:outline focus:outline-secundary-400" />
-    <input v-else v-model="checked" :type="type"
-        class="p-3 rounded-sd bg-neutral-g30 dark:bg-neutral-g500 accent-primary-600 dark:accent-primary-400 text-base lg:text-md focus:outline focus:outline-secundary-400" />
+    <input :name="name" v-if="type !== 'checkbox' && type !== 'radio'" :type="type" v-model="value"
+        class="max-lg:py-3 px-3 py-4 rounded-sd bg-n30 dark:bg-n500 caret-n900 dark:caret-n0 text-neutral-dark dark:text-white text-base lg:text-md focus:outline focus:outline-secundary-400 border-none ring-1 ring-n20 dark:ring-0" />
+    <input :name="name" v-else v-model="checked" :type="type"
+        class="p-3 rounded-sd bg-n30 dark:bg-n500 accent-primary-600 dark:accent-primary-400 text-base lg:text-md focus:outline focus:outline-secundary-400 border-none" />
 </template>
--- pages/login/components/Field.vue
+++ pages/login/components/Field.vue
@@ -4,7 +4,10 @@ const props = defineProps<{
     label: string;
     type: HTMLInputElement['type'];
     passwordToggle?: boolean;
+    labelBellow?: string;
+    inputName?: string;
 }>();
+defineEmits(['enter', 'input', 'change']);
 
 const inputType = ref<HTMLInputElement['type']>('password');
 const togglePassword = () => {
@@ -14,14 +17,26 @@ const lostFocus = () => props.passwordToggle ? inputType.value = 'password' : ''
 </script>
 <template>
     <label @focusout="lostFocus" class="flex flex-col gap-2 text-md lg:text-lg capitalize font-semibold">{{ label }}
-        <g-input class="caret-primary-700 text-primary-900 dark:caret-primary-400 !rounded-sd" v-if="!passwordToggle" :type="type" />
-        <div v-else class="self-stretch relative">
-            <g-input @keyup.enter="$emit('enter')" class="w-full pr-6 caret-primary-700 text-primary-900 dark:caret-primary-400 !rounded-sd" :type="inputType" />
-            <button @focusout="inputType = 'password'" type="button" @click.prevent="togglePassword"
-                class="absolute text-center inline-flex items-center py-2 px-3 top-0 h-full right-0 text-current rounded-sd">
-                <g-icon v-if="inputType === 'password'" class="pointer-events-none text-sm lg:text-md" icon="uil:eye-slash" />
-                <g-icon v-else class="pointer-events-none text-sm lg:text-md" icon="uil:eye" />
-            </button>
+        <g-input v-if="!passwordToggle" :name="inputName" data-input @input="$emit('input', $event.target.value)"
+            class="caret-primary-700 text-primary-900 dark:caret-primary-400 !rounded-sd" :type="type" />
+        <div data-label-bellow v-if="!passwordToggle && labelBellow"
+            class="text-base lg:text-md font-normal text-n900 dark:text-n0">{{ labelBellow }}</div>
+
+        <div v-if="passwordToggle" class="flex flex-col gap-2 self-stretch relative">
+            <div class="w-full relative">
+                <g-input :name="inputName" data-input @input="$emit('input', $event.target.value)"
+                    @keyup.enter="$emit('enter')"
+                    class="w-full pr-6 caret-primary-700 text-primary-900 dark:caret-primary-400 !rounded-sd focus:!outline-secundary-400 focus:border-0"
+                    :type="inputType" />
+                <button @focusout="inputType = 'password'" type="button" @click.prevent="togglePassword"
+                    class="absolute text-center inline-flex items-center p-3 inset-y-0 h-full right-0 text-current rounded-sd">
+                    <g-icon v-if="inputType === 'password'" class="pointer-events-none text-sm lg:text-md"
+                        icon="uil:eye-slash" />
+                    <g-icon v-else class="pointer-events-none text-sm lg:text-md" icon="uil:eye" />
+                </button>
+            </div>
+            <div data-label-bellow v-if="labelBellow" class="text-base lg:text-md font-normal text-n900 dark:text-n0">{{
+                labelBellow }}</div>
         </div>
     </label>
 </template>
