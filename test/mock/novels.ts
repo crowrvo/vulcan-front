025f1e8182e40db88bf0e294c10cd898ec6ec234
devdouglassilva <devdouglassilva@gmail.com>
2024-03-20 16:56:20 +0000

    Update(novel props): Added new props to novel type

--- test/mock/novels.ts
+++ test/mock/novels.ts
@@ -1,3 +1,5 @@
+import type { URL } from "./navigation";
+
 export type chapterURL = {
     to: string;
     chapterNumber: number;
@@ -7,9 +9,13 @@ export type novelDetails = {
     title: string;
     image: string;
     description: string;
-    visibleAsTopRating?: boolean;
-    lastEnumerateChaptersUploaded?: Array<chapterURL>
+    visibleAsTopRating: boolean;
     novelUrl: string;
+    chaptersCounter: number;
+    rating: 0 | 1 | 2 | 3 | 4 | 5;
+    categories: Array<URL>,
+    bannerUrl?: string,
+    publisher: URL
 }
 
 export const novels: Array<novelDetails> = [{
@@ -17,53 +23,42 @@ export const novels: Array<novelDetails> = [{
     image: "/assets/images/background/capa-vulcan-eu-sou-o-unico-necromante-2.png",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     visibleAsTopRating: true,
-    lastEnumerateChaptersUploaded: [{
-        to: "/",
-        chapterNumber: 12.4
-    }, {
-        to: "/",
-        chapterNumber: 13.4
-    }, {
-        to: "/",
-        chapterNumber: 13
-    }],
-    novelUrl: "/"
+    novelUrl: "/",
+    chaptersCounter: 21,
+    rating: 5,
+    categories: [{ name: "Ação", to: "/categories/action" }, { name: "Necromância", to: "/categories/necromance" }],
+    bannerUrl: "",
+    publisher: { name: "Sxka", to: "" }
 }, {
     title: "The Great Gatsby",
     image: "/assets/images/background/capa-vulcan-eu-sou-o-unico-necromante-2.png",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     visibleAsTopRating: true,
-    lastEnumerateChaptersUploaded: [{
-        to: "/",
-        chapterNumber: 23.1
-    }],
-    novelUrl: "/"
+    novelUrl: "/",
+    chaptersCounter: 112,
+    rating: 3,
+    categories: [{ name: "Ação", to: "/categories/action" }, { name: "Comedia", to: "/categories/comedy" }],
+    publisher: { name: "One", to: "" }
 }, {
     title: "Eu sou o único necromante",
     image: "/assets/images/background/capa-vulcan-eu-sou-o-unico-necromante-2.png",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     visibleAsTopRating: true,
-    lastEnumerateChaptersUploaded: [{
-        to: "/",
-        chapterNumber: 2
-    }],
-    novelUrl: "/"
+    novelUrl: "/",
+    chaptersCounter: 31,
+    rating: 4,
+    categories: [{ name: "Musical", to: "/categories/musical" }],
+    publisher: { name: "Ssaa", to: "" }
 }, {
     title: "The Great Gatsby",
     image: "/assets/images/background/capa-vulcan-eu-sou-o-unico-necromante-2.png",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     visibleAsTopRating: false,
-    lastEnumerateChaptersUploaded: [{
-        to: "/",
-        chapterNumber: 23
-    }, {
-        to: "/",
-        chapterNumber: 24
-    }, {
-        to: "/",
-        chapterNumber: 25
-    }],
-    novelUrl: "/"
+    novelUrl: "/",
+    chaptersCounter: 32,
+    rating: 3,
+    categories: [{ name: "Ação", to: "/categories/action" }, { name: "Necromância", to: "/categories/necromance" }],
+    publisher: { name: "Szi", to: "" }
 }];
 
-export const topNovels:Array<novelDetails> = novels.filter(novel => novel.visibleAsTopRating);
+export const topNovels: Array<novelDetails> = novels.filter(novel => novel.visibleAsTopRating);
