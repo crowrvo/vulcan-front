0b978a3392978b04661df82cac8213ac9bb05f62
devdouglassilva <devdouglassilva@gmail.com>
2024-03-21 20:46:35 +0000

    Style(tailwind.config): Add tokens of shadcn Add keyframe tokens of shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -164,7 +164,21 @@ export default {
       aspectRatio: {
         "portrait": "9/16"
       },
-    },
+      keyframes: {
+        "accordion-down": {
+          from: { height: '0' },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    }
   },
   plugins: [require("@tailwindcss/forms")],
 } satisfies Config
