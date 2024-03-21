2708b4a19d7a12f436806eb45cefe89a5c19b14b
devdouglassilva <devdouglassilva@gmail.com>
2024-03-21 22:51:52 +0000

    Feat(home page): Add the new components - Add the Banner component; - Add the NovelList component

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -12,6 +12,9 @@ const route = useRoute();
     <div class="grid grid-cols-2 gap-x-6 gap-y-20 mx-6 lg:mx-16">
       <TopNovels class="col-span-2" />
       <Novels class="col-span-2" />
+      <Banner class="col-span-2" />
+      <NovelList class="col-span-2" filter="national" title="Novels Brasileiras" />
+      <NovelList class="col-span-2" filter="foreign" title="Novels Extrangeiras" />
     </div>
   </div>
 </template>
