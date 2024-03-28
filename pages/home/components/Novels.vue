8db71d1d105801728ab70c9c49d26002cb211c1d
devdouglassilva <devdouglassilva@gmail.com>
2024-03-28 00:06:35 +0000

    Refactor(Novels): Use only necessary props

--- pages/home/components/Novels.vue
+++ pages/home/components/Novels.vue
@@ -39,20 +39,22 @@ import { novels } from '~/test/mock/novels';
                     </p>
                 </div>
                 <div class="flex flex-col gap-2 px-6 pb-6">
-                    <NuxtLink class="text-n0 text-center px-6 py-2 rounded-[16px] bg-secundary-400 w-full transition-[background, outline] duration-300 ease-in text-base hover:active:outline hover:active:bg-secundary-500 outline-secundary-400 focus:outline-secundary-400 focus:outline-offset-1" :to="novels[0].novelUrl">
+                    <NuxtLink
+                        class="text-n0 text-center px-6 py-2 rounded-[16px] bg-secundary-400 w-full transition-[background, outline] duration-300 ease-in text-base hover:active:outline hover:active:bg-secundary-500 outline-secundary-400 focus:outline-secundary-400 focus:outline-offset-1"
+                        :to="novels[0].novelUrl">
                         Conferir</NuxtLink>
-                    <button tabindex="0" class="text-n0 bg-n500 px-6 py-2 rounded-[16px] text-left inline-flex gap-1 w-full select-none ease-in hover:active:outline transition-[background, outline] duration-300 hover:active:bg-n600 outline-n400 focus:outline-n400 focus:outline-offset-1">
+                    <button tabindex="0"
+                        class="text-n0 bg-n500 px-6 py-2 rounded-[16px] text-left inline-flex gap-1 w-full select-none ease-in hover:active:outline transition-[background, outline] duration-300 hover:active:bg-n600 outline-n400 focus:outline-n400 focus:outline-offset-1">
                         <g-icon class="hidden lg:block text-base" icon="uil:books" />
                         Adicionar a leitura
                     </button>
                 </div>
             </div>
             <div class="flex flex-wrap px-6 gap-6 self-stretch">
-                <g-novel-card class="h-min" show-as-variant="portrait" :banner-url="novel.bannerUrl ?? ''" :publisher="novel.publisher"
-                    v-for="novel in novels.slice(1, 11)" :title="novel.title" :image="novel.image"
-                    :novel-url="novel.novelUrl" :visible-as-top-rating="true" :description="novel.description"
-                    :rating="novel.rating" :show-chapters="true" :chapters-counter="novel.chaptersCounter"
-                    :show-rating="false" :categories="novel.categories" />
+                <g-novel-card class="h-min" show-as-variant="portrait" v-for="novel in novels.slice(1, 11)"
+                    :key="novel.novelUrl" :title="novel.title" :image="novel.image" :novel-url="novel.novelUrl"
+                    :description="novel.description" :rating="novel.rating" :show-chapters="true"
+                    :chapters-counter="novel.chaptersCounter" :categories="novel.categories" />
             </div>
         </div>
     </section>
